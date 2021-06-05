e60c76d893532bc473440db34fa52def0ef02270
ankushduacodes <ankushduacodes@gmail.com>
2021-06-05 19:10:21 +0530

    setup basic server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+}).on('error', (err) => {
+  console.log(err);
+});
