4ed72bae95c72dad39e939571a8ecc5bec5fe3f2
ankushduacodes <ankushduacodes@gmail.com>
2021-06-20 02:53:48 +0530

    added cors to the api

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { db } from './models/db';
 
 import coaches from './routes/coaches';
@@ -13,6 +14,27 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+const allowedOrigins = [
+  'http://localhost:8081',
+];
+
+const corsOptions = {
+  // @ts-ignore
+  origin: (origin: string, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 204,
+};
+
+// @ts-ignore
+app.options('*', cors(corsOptions));
+// @ts-ignore
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.get('/favicon.ico', (req: Request, res: Response) => res.sendStatus(204));
 app.use(`${basePath}/coaches`, coaches);
@@ -24,4 +46,4 @@ app.listen(port, () => {
   console.log(err);
 });
 
-console.log(db);
+console.log(Object.entries(db)[-1]);
