962fe26d32a58abefc7dd531c5252eefc744dbdb
ankushduacodes <ankushduacodes@gmail.com>
2021-06-22 18:37:36 +0530

    removed use of cors as determined there was no need for it

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import { db } from './models/db';
 
 import coaches from './routes/coaches';
@@ -10,32 +9,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 const basePath = '/api/v1';
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-const allowedOrigins = [
-  'http://localhost:8081',
-];
-
-const corsOptions = {
-  // @ts-ignore
-  origin: (origin: string, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Origin not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 204,
-};
-
-// @ts-ignore
-app.options('*', cors(corsOptions));
-// @ts-ignore
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
 app.get('/favicon.ico', (req: Request, res: Response) => res.sendStatus(204));
 app.use(`${basePath}/coaches`, coaches);
 app.use(`${basePath}/requests`, requests);
